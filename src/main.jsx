80e165d9c00338a6dbd1245c698e5830cfadfc1a
Kartik <iamkaranmishra551@gmail.com>
2024-03-07 22:43:53 +0530

    Adding basic structure.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import  ReactDOM  from 'react-dom';
+function App() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
